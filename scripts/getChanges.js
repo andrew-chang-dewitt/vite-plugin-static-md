64989454fd825eacc80f314217f14609d084c152
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2024-11-12 12:39:29 -0600

    ci: parse changelog for release body

---
+++ scripts/getChanges.js
@@ -0,0 +1,37 @@
+import { readFile, writeFile } from "node:fs/promises"
+import { resolve } from "node:path"
+
+const __dirname = import.meta.dirname
+const CHANGELOG = resolve(__dirname, "../CHANGELOG.md")
+const BODYFILE = resolve(__dirname, "../BODY.md")
+
+async function main() {
+  const file = await readFile(CHANGELOG, { encoding: "utf8" })
+  const lines = file.split(/\r\n|\n/)
+
+  let res = ``
+  let take = false
+  let keepGoing = true
+  let idx = 0
+
+  while (keepGoing) {
+    const line = lines[idx]
+
+    if (line.startsWith("## [") && !take) {
+      take = true
+    } else if (line.startsWith("## [") && take) {
+      take = false
+      keepGoing = false
+    } else if (take) {
+      res += line
+      res += "\n"
+    }
+
+    idx++
+  }
+
+  console.log(`writing to ${BODYFILE}`)
+  await writeFile(BODYFILE, res.trim(), { encoding: "utf8" })
+}
+
+main()
