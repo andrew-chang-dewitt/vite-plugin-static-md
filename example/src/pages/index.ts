896447cbe4374983e48b23051e43222f91b9d0ca
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2025-01-30 09:37:31 -0600

    docs(ctx): update example to use new ctx obj

--- example/src/pages/index.ts
+++ example/src/pages/index.ts
@@ -1,25 +1,31 @@
 /// A POC of how to use the context object in dev to dynamically build lists of pages
+import type { Context, Page } from "vite-plugin-static-md"
+
+declare global {
+  interface Document {
+    ctx: Context
+  }
+}
 
 // get pages as a list of (url, data)
-const pages = await fetch("/__/pages/").then((res) => res.json())
-const pageList = Object.entries(pages)
+const pages = (document["ctx"] as Context).pages
+console.dir(pages)
 
 // get target ul el
 const ul = document.querySelector("#toc > ul")
 
 // iterate over page list & build list items
-function buildEntryLi(el: [string, any]): HTMLLIElement {
-  const [url, page] = el
+function buildEntryLi(page: Page): HTMLLIElement {
   const li = document.createElement("li")
   const an = document.createElement("a")
   li.appendChild(an)
-  an.href = url
+  an.href = page.url
   an.textContent = page.data.title
 
   return li
 }
 
-for (let page of pageList) {
+for (let page of Object.values(pages)) {
   ul?.appendChild(buildEntryLi(page))
 }
 
--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,7 +1,7 @@
 import { resolve } from "path"
 import type { UserConfig } from "vite"
 
-import staticMd from "../src/main.js"
+import staticMd from "vite-plugin-static-md"
 
 const OUT_DIR = resolve(__dirname, "dist")
 const HTML_ROOT = resolve(__dirname, "src/pages")
